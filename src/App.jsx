722f8b0dd68e81f06635cf726745190b2db9ddb3
ddaviddahlgren <ddaviddahlgren@gmail.com>
2025-12-05 09:44:04 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Router>
+      <Routes>
+        {/* David gör Habits <Route></Route> */}
+        {/* Manau gör Todos <Route></Route> */}
+        {/* Oscar gör Event Planner <Route></Route> */}
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
