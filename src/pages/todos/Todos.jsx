e4c424573a794794041d4672fa93a26e3bd20f5d
ManauTunjae <manau.tunjae@hotmail.com>
2025-12-05 19:49:22 +0100

    feat: Create useState hooks for todos and input fields.

--- src/pages/todos/Todos.jsx
+++ src/pages/todos/Todos.jsx
@@ -1,8 +1,19 @@
-const Todos =()=>{
-    return(
-        <>
-        <p>Todos Page</p>
-        </>
-    );
-}
+import { useEffect, useState } from "react";
+
+const Todos = () => {
+  const [todos, setTodos] = useState([]);
+  // useState för att spara värde input värdet
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState(""); 
+  const [time, setTime] = useState("");
+  const [category, setCategory] = useState("");
+  const [deadline, setDeadline] = useState("");
+
+  return (
+    <div>
+      <h3>Todos Page</h3>
+
+    </div>
+  );
+};
 export default Todos;
