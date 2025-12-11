7ad400a9da1fa950010c968cdafb63fa40d65805
ManauTunjae <manau.tunjae@hotmail.com>
2025-12-11 19:58:27 +0100

    feat: import TodoProvider to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,26 @@ import Todos from './pages/todos/Todos'
 import Events from './pages/events/Events'
 import { EventProvider } from './context/EventContext'
 import Habits from './pages/habits/Habits'
-
+import { TodoProvider } from './context/todos/TodoContext'
 
 function App() {
 
   return (
     <>
     <EventProvider>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path="/habits" element={<Habits />} />
-        <Route path='/todos' element={<Todos/>} />     {/* Manau gör Todos <Route></Route> */}
-        <Route path='/events' element={<Events/>} />   {/* Oscar gör Event Planner <Route></Route> */}
-        {/* <Route path='*' element={} /> */}
-        {/* Manau gör Todos <Route></Route> */}
-        {/* Oscar gör Event Planner <Route></Route> */}
-      </Routes>
-    </Router>
+      <TodoProvider>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path="/habits" element={<Habits />} />
+            <Route path='/todos' element={<Todos/>} />     {/* Manau gör Todos <Route></Route> */}
+            <Route path='/events' element={<Events/>} />   {/* Oscar gör Event Planner <Route></Route> */}
+            {/* <Route path='*' element={} /> */}
+            {/* Manau gör Todos <Route></Route> */}
+            {/* Oscar gör Event Planner <Route></Route> */}
+          </Routes>
+        </Router>
+      </TodoProvider>
     </EventProvider>
     </>
   )
