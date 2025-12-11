e027b003f4a6b3322a9d6877ff417f2c96c29182
ManauTunjae <manau.tunjae@hotmail.com>
2025-12-12 00:04:24 +0100

    feat: update returning

--- src/components/todos/todoList/TodoList.jsx
+++ src/components/todos/todoList/TodoList.jsx
@@ -3,25 +3,15 @@ import { TodoContext } from "../../../context/todos/TodoContext";
 import TodoItem from "../todoItem/TodoItem.jsx";
 
 const TodoList = () => {
-  const { filteredTodo, handleToggleStatus } = useContext(TodoContext);
+  const { filteredTodo } = useContext(TodoContext);
 
   return (
     <>
       {/* Loopen använder filteredTodo för att visa den sorterade/filtrerade listan */}
       <h5>Todo-lists</h5>
       {filteredTodo.map((todo) => (
-        <div
-          key={todo.id}
-          className={`${style.todoItem} ${
-            todo.status ? style.todoItemCompleted : ""
-          }`}
-        >
-          <input
-            type="checkbox"
-            checked={todo.status}
-            onChange={() => handleToggleStatus(todo.id)}
-          />
-        </div>
+        <TodoItem 
+        key={todo}/>
       ))}
     </>
   );
