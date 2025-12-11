ba038bce282709e2e229910e1526a728ba7eafad
ManauTunjae <manau.tunjae@hotmail.com>
2025-12-11 20:44:05 +0100

    feat: create children components for todo part

---
+++ src/components/todos/ChooseTodo.jsx
@@ -0,0 +1,7 @@
+const ChooseTodo = () => {
+    return (
+        <>
+        </>
+    )
+}
+export default ChooseTodo;
---
+++ src/components/todos/TodoInput.jsx
@@ -0,0 +1,7 @@
+const TodoInput = () => {
+    return (
+        <>
+        </>
+    )
+}
+export default TodoInput;
