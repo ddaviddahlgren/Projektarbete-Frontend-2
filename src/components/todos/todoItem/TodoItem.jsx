343c94c0e96f2f89bed95e66638d7d11ea085ee5
ManauTunjae <manau.tunjae@hotmail.com>
2025-12-13 17:01:05 +0100

    feat: uppdate rendered elements

--- src/components/todos/todoItem/TodoItem.jsx
+++ src/components/todos/todoItem/TodoItem.jsx
@@ -1,58 +1,99 @@
 import React, { useContext } from "react";
 import { TodoContext } from "../../../context/todos/TodoContext";
+import style from "./todoItem.module.css";
 
-const TodoItem = () => {
+const TodoItem = ({ todo }) => {
   const {
-    todo,
     editTodoId,
     setEditTodoId,
     editTitle,
     setEditTitle,
     editDescription,
     setEditDescription,
+    handleToggleStatus,
     handleDeleteTodo,
     editingTodo,
     saveEdit,
   } = useContext(TodoContext);
 
   return (
-    <>
-      {editTodoId === todo.id ? (
-        <div>
-          <input
-            placeholder="Title"
-            value={editTitle}
-            onChange={(e) => setEditTitle(e.target.value)}
-          />
-
-          <textarea
-            placeholder="Description"
-            value={editDescription}
-            onChange={(e) => setEditDescription(e.target.value)}
-          />
-
-          <button onClick={() => saveEdit(todo.id)}>Save edit ✅</button>
-          <button onClick={() => setEditTodoId(null)}>Cancel ❌</button>
-        </div>
-      ) : (
-        <div>
-          <h6>Title: {todo.title}</h6>
-          <p>Description: {todo.description}</p>
-          <button
-            onClick={() => editingTodo(todo)}
-            className={style.editButton}
-          >
-            Edit ✍🏼
-          </button>
-        </div>
-      )}
-      <button
-        onClick={() => handleDeleteTodo(todo.id)}
-        className={style.deleteButton}
+    <div>
+      <div
+        key={todo.id}
+        className={`${style.todoItem} ${
+          todo.status ? style.todoItemCompleted : ""
+        }`}
       >
-        Delete 🗑️
-      </button>
-    </>
+        {editTodoId === todo.id ? (
+          <div className={style.editTodo}>
+            <h6 className={style.editHeader}>Edit your todo</h6>
+            <label for="editTitle">
+              Edit todo:
+            <input
+              placeholder="Todo"
+              value={editTitle}
+              onChange={(e) => setEditTitle(e.target.value)}
+              className={style.editTitle}
+            />
+            </label>
+
+            <label for="editDescription">
+              Edit note:
+            <textarea
+              placeholder="Edit note"
+              value={editDescription}
+              onChange={(e) => setEditDescription(e.target.value)}
+              className={style.editDescription}
+            />
+            </label>
+
+            <button
+              onClick={() => saveEdit(todo.id)}
+              className={style.saveEditButton}
+            >
+              Save edit
+            </button>
+            <button
+              onClick={() => setEditTodoId(null)}
+              className={style.cancelEditButton}
+            >
+              Cancel
+            </button>
+          </div>
+        ) : (
+          <div>
+            <div className={style.checkedTodo}>
+              <input
+                type="checkbox"
+                checked={todo.status}
+                onChange={() => handleToggleStatus(todo.id)}
+              />
+              <p>Status: {!todo.status ? 'In progress': 'Checked'}</p>
+            </div>
+            <h6>Todo: {todo.title}</h6>
+            <p>Note: {todo.description}</p>
+            <p>🗓️ {todo.deadline}</p>
+            <p>
+              ⏰ {todo.hours} hr. {todo.minutes} min.
+            </p>
+            <div className={style.buttonContainer}>
+              <button
+                onClick={() => editingTodo(todo)}
+                className={style.editButton}
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => handleDeleteTodo(todo.id)}
+                className={style.deleteButton}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 export default TodoItem;
