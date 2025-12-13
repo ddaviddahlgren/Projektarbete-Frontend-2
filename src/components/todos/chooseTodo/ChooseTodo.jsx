41e7c6d495b0b0f11d80631c5a221e70cbba540c
ManauTunjae <manau.tunjae@hotmail.com>
2025-12-13 17:00:01 +0100

    feat: add className to styling

--- src/components/todos/chooseTodo/ChooseTodo.jsx
+++ src/components/todos/chooseTodo/ChooseTodo.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { TodoContext } from "../../../context/todos/TodoContext";
+import style from "./chooseTodo.module.css"
 
 const ChooseTodo = () => {
   const {
@@ -12,16 +13,16 @@ const ChooseTodo = () => {
   } = useContext(TodoContext);
   return (
     <div>
-      <div>
-        <h5>Choose by category: </h5>
+      <div className={style.chooseCat}>
+        <h3>Choose by category</h3>
         {categories.map((category, i) => (
-          <button key={i} onClick={() => handleFilterCategory(category)}>
+          <button key={i} onClick={() => handleFilterCategory(category)} className={style.catButton}>
             {category}
           </button>
         ))}
       </div>
-      <div>
-        <h5>Choose by status: </h5>
+      <div className={style.chooseStatus}>
+        <h3>Choose by status</h3>
         <select
           onChange={(e) => setFilterStatus(e.target.value)}
           value={filterStatus}
@@ -30,6 +31,7 @@ const ChooseTodo = () => {
           <option value="Checked">Checked</option>
           <option value="In progress">In progress</option>
         </select>
+        
         <select onChange={(e) => setSortBy(e.target.value)} value={sortBy}>
           <option value="">Sort By</option>
           <option value="deadline-asc">Deadline (Tidigast först)</option>
