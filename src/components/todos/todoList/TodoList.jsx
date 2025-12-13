88bdbd13df38a381092573370e4b91b690af364f
ManauTunjae <manau.tunjae@hotmail.com>
2025-12-13 16:58:24 +0100

    feat: add div-el & className to styling

--- src/components/todos/todoList/TodoList.jsx
+++ src/components/todos/todoList/TodoList.jsx
@@ -1,19 +1,21 @@
 import React, { useContext } from "react";
 import { TodoContext } from "../../../context/todos/TodoContext";
 import TodoItem from "../todoItem/TodoItem.jsx";
+import style from "./todoList.module.css";
 
 const TodoList = () => {
   const { filteredTodo } = useContext(TodoContext);
 
   return (
-    <>
-      {/* Loopen använder filteredTodo för att visa den sorterade/filtrerade listan */}
-      <h5>Todo-lists</h5>
-      {filteredTodo.map((todo) => (
-        <TodoItem 
-        key={todo}/>
-      ))}
-    </>
+    <div>
+      <h3>Todo-lists</h3>
+      <div className={style.todoContainer}>
+        {/* Loopen använder filteredTodo för att visa den sorterade/filtrerade listan */}
+        {filteredTodo.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </div>
+    </div>
   );
 };
 export default TodoList;
