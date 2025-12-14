b6ebc592a3870aeacda0ce4659906d387f201702
ManauTunjae <manau.tunjae@hotmail.com>
2025-12-14 17:08:02 +0100

    feat: update elements render

--- src/components/todos/chooseTodo/ChooseTodo.jsx
+++ src/components/todos/chooseTodo/ChooseTodo.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import { TodoContext } from "../../../context/todos/TodoContext";
-import style from "./chooseTodo.module.css"
+import style from "./chooseTodo.module.css";
 
 const ChooseTodo = () => {
   const {
     categories,
     handleFilterCategory,
+    selectedCategories,
     filterStatus,
     setFilterStatus,
     sortBy,
@@ -15,30 +16,48 @@ const ChooseTodo = () => {
     <div>
       <div className={style.chooseCat}>
         <h3>Choose by category</h3>
-        {categories.map((category, i) => (
-          <button key={i} onClick={() => handleFilterCategory(category)} className={style.catButton}>
-            {category}
-          </button>
-        ))}
+        <div className={style.catBtnContainer}>
+          {categories.map((category, i) => (
+            <button
+              key={i}
+              onClick={() => handleFilterCategory(category)}
+              className={`${style.catButton} ${
+                selectedCategories.includes(category) ? style.activeCat : " "
+              }`}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
       </div>
       <div className={style.chooseStatus}>
         <h3>Choose by status</h3>
-        <select
-          onChange={(e) => setFilterStatus(e.target.value)}
-          value={filterStatus}
-        >
-          <option value="All Todos">All status</option>
-          <option value="Checked">Checked</option>
-          <option value="In progress">In progress</option>
-        </select>
-        
-        <select onChange={(e) => setSortBy(e.target.value)} value={sortBy}>
-          <option value="">Sort By</option>
-          <option value="deadline-asc">Deadline (Tidigast först)</option>
-          <option value="deadline-desc">Deadline (Senast först)</option>
-          <option value="time-asc">Tid (Kortast först)</option>
-          <option value="time-desc">Tid (Längst först)</option>
-        </select>
+        <div className={style.sortBySelect}>
+          <div>
+            <label>Sort by status</label>
+            <select
+              onChange={(e) => setFilterStatus(e.target.value)}
+              value={filterStatus}
+            >
+              <option value="All Todos">All status</option>
+              <option value="Checked">Checked</option>
+              <option value="In progress">In progress</option>
+            </select>
+          </div>
+
+          <div>
+            <label>Sort by deadline & Time</label>
+            <select
+              onChange={(e) => setSortBy(e.target.value)}
+              value={sortBy || ""}
+            >
+              <option value="deadline-asc">Deadline: (Earliest First)</option>
+              <option value="deadline-desc">Deadline: (Latest First)</option>
+              <option value="time-asc">Time: (Shortest First)</option>
+              <option value="time-desc">Time: (Longest First)</option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   );
