1425cce4850c64625b719f385a0afa38f3474f15
ManauTunjae <manau.tunjae@hotmail.com>
2025-12-15 17:19:22 +0100

    feat: add back to home

--- src/pages/todos/Todos.jsx
+++ src/pages/todos/Todos.jsx
@@ -1,17 +1,22 @@
-import React from "react";
 import ChooseTodo from "../../components/todos/chooseTodo/ChooseTodo.jsx";
 import TodoInput from "../../components/todos/todoInput/TodoInput.jsx";
 import TodoList from "../../components/todos/todoList/TodoList.jsx";
 import style from "./Todos.module.css";
+import { Link } from "react-router-dom";
 
 const Todos = () => {
   return (
     <>
       <div>
         <h2 className={style.todoPageHeader}>Todo Page</h2>
+        <div className={style.backHome}>
+          <Link to="/" className={style.backHomeLink}>
+            ⬅️ BACK TO HOME
+          </Link>
+        </div>
         <div className={style.todoPage}>
-        <TodoInput />
-        <ChooseTodo />
+          <TodoInput />
+          <ChooseTodo />
         </div>
         <TodoList />
       </div>
