5859d5b703c022e8e8fbc09b8afe0ee3862664e8
Oscar <o.frykedal@gmail.com>
2025-12-18 11:13:02 +0100

    added username safety

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -16,16 +16,26 @@ const Login = () => {
   } = useContext(UserContext);
 
   const registerUser = () => {
-    const newUser = {
-      username: username,
-      password: password,
-      id: users.length + 1,
-      habits: [],
-      todos: [],
-      events: [],
-    };
 
-    setUsers([...users, newUser]);
+    if(users.find((u) => u.username === username)){
+      alert("User already exists");
+      setPassword("");
+      setUsername("");
+      return;
+    }else{ 
+      alert("User registered, please login"); 
+        const newUser = {
+        username: username,
+        password: password,
+        id: users.length + 1,
+        habits: [],
+        todos: [],
+        events: [],
+      };
+
+      setUsers([...users, newUser]);
+
+    }
   };
 
   const loginUser = () => {
@@ -50,6 +60,7 @@ const Login = () => {
         <input
           type="text"
           placeholder="Username"
+          value={username} 
           autoComplete="username"
           onChange={(e) => {
             setUsername(e.target.value);
@@ -59,6 +70,7 @@ const Login = () => {
         <input
           type="password"
           placeholder="Password"
+          value={password} 
           autoComplete="current-password"
           onChange={(e) => {
             setPassword(e.target.value);
