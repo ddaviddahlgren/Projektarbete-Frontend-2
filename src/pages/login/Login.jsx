52236e5835dcee4c4ccae1ed2fd8219bb5cf3ba5
Oscar <o.frykedal@gmail.com>
2025-12-18 12:33:55 +0100

    formatting

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -6,26 +6,17 @@ import style from "./Login.module.css";
 const Login = () => {
   const navigate = useNavigate();
 
-  const {
-    users,
-    setUsers,
-    setLoggedInUser,
-    username,
-    setUsername,
-    password,
-    setPassword,
-  } = useContext(UserContext);
+  const { users, setUsers, setLoggedInUser, username, setUsername, password, setPassword } = useContext(UserContext);
 
   const registerUser = () => {
-
-    if(users.find((u) => u.username === username)){
+    if (users.find((u) => u.username === username)) {
       alert("User already exists");
       setPassword("");
       setUsername("");
       return;
-    }else{ 
-      alert("User registered, please login"); 
-        const newUser = {
+    } else {
+      alert("User registered, please login");
+      const newUser = {
         username: username,
         password: password,
         id: users.length + 1,
@@ -35,36 +26,33 @@ const Login = () => {
       };
 
       setUsers([...users, newUser]);
-
     }
   };
 
   const loginUser = () => {
-    const user = users.find(
-      (u) => u.username === username && u.password === password
-    );
-    if(user == undefined){
-        alert("User not found, please register");
-        return;
-    }else{
-        setLoggedInUser(user)
-        sessionStorage.setItem('justLoggedIn', 'true'); //sessionstorage to only show quote on login
-        sessionStorage.removeItem('welcomeShown'); // Clear previous welcome
-        navigate("/home");
+    const user = users.find((u) => u.username === username && u.password === password);
+    if (user == undefined) {
+      alert("User not found, please register");
+      return;
+    } else {
+      setLoggedInUser(user);
+      sessionStorage.setItem("justLoggedIn", "true"); //sessionstorage to only show quote on login
+      sessionStorage.removeItem("welcomeShown"); // Clear previous welcome
+      navigate("/home");
     }
   };
 
   return (
     <div>
       <h1 className={style.homeTitle}>
-                PRODUCTIVITY <span id={style.titleSpan}>ASSISTANT</span>
-              </h1>
-              <p id={style.creditsText}>By Manau, Oscar & David</p>
+        PRODUCTIVITY <span id={style.titleSpan}>ASSISTANT</span>
+      </h1>
+      <p id={style.creditsText}>By Manau, Oscar & David</p>
       <form>
         <input
           type="text"
           placeholder="Username"
-          value={username} 
+          value={username}
           autoComplete="username"
           onChange={(e) => {
             setUsername(e.target.value);
@@ -74,7 +62,7 @@ const Login = () => {
         <input
           type="password"
           placeholder="Password"
-          value={password} 
+          value={password}
           autoComplete="current-password"
           onChange={(e) => {
             setPassword(e.target.value);
