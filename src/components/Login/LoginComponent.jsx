07d557def4cf3c47bd6615f96bc6513fcc4b9b1c
ddaviddahlgren <ddaviddahlgren@gmail.com>
2025-12-18 12:28:29 +0100

    CSS

---
+++ src/components/Login/LoginComponent.jsx
@@ -0,0 +1,103 @@
+import { useContext } from "react";
+import { UserContext } from "../../context/users/UserContext";
+import { useNavigate } from "react-router-dom";
+import style from "../../pages/login/Login.module.css";
+
+export default function LoginComponent() {
+  const navigate = useNavigate();
+
+  const {
+    users,
+    setUsers,
+    setLoggedInUser,
+    username,
+    setUsername,
+    password,
+    setPassword,
+  } = useContext(UserContext);
+
+  const registerUser = () => {
+    if (users.find((u) => u.username === username)) {
+      alert("User already exists");
+      setPassword("");
+      setUsername("");
+      return;
+    } else {
+      alert("User registered, please login");
+      const newUser = {
+        username: username,
+        password: password,
+        id: users.length + 1,
+        habits: [],
+        todos: [],
+        events: [],
+      };
+
+      setUsers([...users, newUser]);
+    }
+  };
+
+  const loginUser = () => {
+    const user = users.find(
+      (u) => u.username === username && u.password === password
+    );
+    if (user == undefined) {
+      alert("User not found, please register");
+      return;
+    } else {
+      setLoggedInUser(user);
+      sessionStorage.setItem("justLoggedIn", "true"); //sessionstorage to only show quote on login
+      sessionStorage.removeItem("welcomeShown"); // Clear previous welcome
+      navigate("/home");
+    }
+  };
+
+  return (
+    <>
+      <div className={style.header}>
+        <h1 className={style.homeTitle}>
+          PRODUCTIVITY <span id={style.titleSpan}>ASSISTANT</span>
+        </h1>
+        <p id={style.creditsText}>By Manau, Oscar & David</p>
+      </div>
+      <h2 className={style.loginTitle}>
+        Please login with your username, <br />
+        or register if you are a new user!
+      </h2>
+      <div className={style.loginContainer}>
+        <form>
+          <input
+            className={style.loginInput}
+            type="text"
+            placeholder="Username"
+            value={username}
+            autoComplete="username"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <br />
+          <input
+            className={style.loginPass}
+            type="password"
+            placeholder="Password"
+            value={password}
+            autoComplete="current-password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <br />
+        </form>
+        <div className={style.loginBtnBox}>
+          <button className={style.loginBtn} onClick={registerUser}>
+            Register
+          </button>
+          <button className={style.loginBtn} onClick={loginUser}>
+            Login
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/home/homeComponent/HomeComponent.jsx
+++ src/components/home/homeComponent/HomeComponent.jsx
@@ -5,43 +5,37 @@ import { Link } from "react-router-dom";
 import style from "./HomeComponent.module.css";
 import WelcomeHome from "../welcomeHome/WelcomeHome";
 
-const HomeContext =()=>{
+const HomeContext = () => {
+  const { loggedInUser } = useContext(UserContext);
 
-    const { loggedInUser } = useContext(UserContext);
-    
-    //check if just logged in to show quote
-    const [showWelcome, setShowWelcome] = useState(() => {
-        return sessionStorage.getItem('justLoggedIn') === 'true';
-    });
+  //check if just logged in to show quote
+  const [showWelcome, setShowWelcome] = useState(() => {
+    return sessionStorage.getItem("justLoggedIn") === "true";
+  });
 
-    const habits = loggedInUser?.habits || []
-    const topHabits = [...habits]
-    .sort((a, b) => b.reps - a.reps)
+  const habits = loggedInUser?.habits || [];
+  const topHabits = [...habits].sort((a, b) => b.reps - a.reps).slice(0, 3);
+
+  const todos = loggedInUser?.todos || [];
+  const topTodos = [...todos]
+    .filter((todo) => todo.status === false)
     .slice(0, 3);
-    
-    const todos = loggedInUser?.todos || []
-    const topTodos = [...todos]
-    .filter(todo => todo.status === false)
-    .slice(0, 3)
 
-    const events = loggedInUser?.events || []
-    const topEvents = [...events]
-    .slice(0, 3)
+  const events = loggedInUser?.events || [];
+  const topEvents = [...events].slice(0, 3);
 
-    const handleWelcomeComplete = () => {
-        setShowWelcome(false);
-        sessionStorage.removeItem('justLoggedIn'); // Clear the flag
-        sessionStorage.removeItem('quoteFetched'); // Clear for next login
+  const handleWelcomeComplete = () => {
+    setShowWelcome(false);
+    sessionStorage.removeItem("justLoggedIn"); // Clear the flag
+    sessionStorage.removeItem("quoteFetched"); // Clear for next login
+  };
 
-    };
-  
-    return(
-        <div>
-            {showWelcome && <WelcomeHome onComplete={handleWelcomeComplete} />}
+  return (
+    <div>
+      {showWelcome && <WelcomeHome onComplete={handleWelcomeComplete} />}
 
-            <BackButton />
-            <h2 > Welcome {loggedInUser?.username}!</h2>
-             <header className={style.header}>
+      <h2 className={style.welcomeText}> Welcome {loggedInUser?.username}!</h2>
+      <header className={style.header}>
         <h1 className={style.homeTitle}>
           PRODUCTIVITY <span id={style.titleSpan}>ASSISTANT</span>
         </h1>
@@ -49,104 +43,68 @@ const HomeContext =()=>{
       </header>
       <main className={style.main}>
         <div className={style.componentContainer}>
-        <h3 className={style.componentTitle}>HABITS</h3>
-        <Link to="/habits">
-          <div className={style.homeContainer}>
-            {topHabits.length === 0 && <p className={style.homeLists}>No habits yet</p>}
+          <h3 className={style.componentTitle}>HABITS</h3>
+          <Link to="/habits">
+            <div className={style.homeContainer}>
+              {topHabits.length === 0 && (
+                <p className={style.homeLists}>No habits yet</p>
+              )}
 
-            <ul>
-              {topHabits.map((habit) => (
-                <li key={habit.id} className={style.homeLists}>
-                  {habit.title} <br/>
-                  <span id={style.statusText}>Reps:</span> {habit.reps} reps
-                </li>
-              ))}
-            </ul>
-          </div>
-        </Link>
+              <ul>
+                {topHabits.map((habit) => (
+                  <li key={habit.id} className={style.homeLists}>
+                    {habit.title} <br />
+                    <span id={style.statusText}>Reps:</span> {habit.reps} reps
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Link>
         </div>
         <br />
         <div className={style.componentContainer}>
-        <h3 className={style.componentTitle}>TODOS</h3>
-        <Link to="/todos">
-          <div className={style.homeContainer}>
-            {topTodos.length === 0 && <p className={style.homeLists}>No todos yet</p>}
-            <ul>
-              {topTodos.map((todo) => (
-                <li key={todo.id} className={style.homeLists}>
-                  {todo.title} <br/> 
-                  <span id={style.statusText}>Status:</span> {todo.status}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </Link>
+          <h3 className={style.componentTitle}>TODOS</h3>
+          <Link to="/todos">
+            <div className={style.homeContainer}>
+              {topTodos.length === 0 && (
+                <p className={style.homeLists}>No todos yet</p>
+              )}
+              <ul>
+                {topTodos.map((todo) => (
+                  <li key={todo.id} className={style.homeLists}>
+                    {todo.title} <br />
+                    <span id={style.statusText}>Status:</span> {todo.status}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Link>
         </div>
         <br />
         <div className={style.componentContainer}>
-        <h3 className={style.componentTitle}>EVENTS</h3>
-        <Link to="/events">
-          <div className={style.homeContainer}>
-            {topEvents.length === 0 && <p className={style.homeLists}>No events yet</p>}
-            <ul>
-              {topEvents.map((event) => (
-                <li key={event.id} className={style.homeLists}>
-                  {event.name} <br/>
-                  <span id={style.statusText}>Date:</span> {event.date}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </Link>
+          <h3 className={style.componentTitle}>EVENTS</h3>
+          <Link to="/events">
+            <div className={style.homeContainer}>
+              {topEvents.length === 0 && (
+                <p className={style.homeLists}>No events yet</p>
+              )}
+              <ul>
+                {topEvents.map((event) => (
+                  <li key={event.id} className={style.homeLists}>
+                    {event.name} <br />
+                    <span id={style.statusText}>Date:</span> {event.date}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Link>
         </div>
       </main>
-            {/* <nav>
-                <Link to="/habits">
-                <div className={style.homeContainer}>
-                    <h3>TOP 3 HABITS</h3>
-                    {topHabits.length === 0 && <p>No habits yet</p>}
-
-                    <ul>
-                    {topHabits.map((habit) => (
-                        <li key={habit.id}>
-                        {habit.title} – {habit.reps} reps
-                        </li>
-                    ))}
-                    </ul>
-                </div>
-                </Link>
-                <br />
-                <Link to="/todos">
-                <div className={style.homeContainer}>
-                    <h3>TOP 3 TODOS</h3>
-                    {topTodos.length === 0 && <p>No todos yet</p>}
-                    <ul>
-                    {topTodos.map((todo) => (
-                        <li key={todo.id}>
-                        {todo.title} - Status: {todo.status}
-                        </li>
-                    ))}
-                    </ul>
-                </div>
-                </Link>
-                <br />
-                <Link to="/events">
-                <div className={style.homeContainer}>
-                    <h3>TOP 3 EVENTS</h3>
-                    {topEvents.length === 0 && <p>No events yet</p>}
-                    <ul>
-                    {topEvents.map((event) => (
-                        <li key={event.id}>
-                        {event.name} - Date: {event.date}
-                        </li>
-                    ))}
-                    </ul>
-                    </div>
-                </Link>
-            </nav> */}
-        </div>
-    );
-
-}
+      <footer>
+        <BackButton/>
+      </footer>
+    </div>
+  );
+};
 
 export default HomeContext;
